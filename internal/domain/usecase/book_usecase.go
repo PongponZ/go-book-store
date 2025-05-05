2828a111e0a075d60ad3be6f697713d91f43261f
PongponZ <mr.pongpon.sinlapa.m.m@gmail.com>
2025-05-06 01:42:40 +0700

    feat: add some error code

--- internal/domain/usecase/book_usecase.go
+++ internal/domain/usecase/book_usecase.go
@@ -1,9 +1,25 @@
 package usecase
 
+import (
+	"fmt"
+)
+
 type IBookUsecase interface{}
 
 type bookUsecase struct{}
 
 func NewBookUsecase() IBookUsecase {
 	return &bookUsecase{}
+}
+
+func (u *bookUsecase) SomeFunc1(bookType string) {
+	if bookType == "fiction" {
+		fmt.Println("hi, SomeFunc1")
+	}
+}
+
+func (u *bookUsecase) SomeFunc2(bookType string) {
+	if bookType == "fiction" {
+		fmt.Println("hi, SomeFunc2")
+	}
 }
